de7e53e9f369096394854628658368cbd97ebd62
David <David@LAPTOP-NBGKI78E>
2019-10-20 10:20:39 +0200

    Air

--- js/map.js
+++ js/map.js
@@ -1,19 +1,13 @@
-const copyright =
-    "'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, " +
-    "<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " +
-    "Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',";
-
-const leafyToken = "pk.eyJ1IjoibWFpbm5pbmUiLCJhIjoiY2sxeGg2cDQ3MGJtdjNvcWd4aWplMnFtMCJ9.6QkdiBXn6IMmw8MFEe455g";
-
-
 $.get("http://172.16.3.118:5000/garbages", data => {
     let map = L.map('map').setView([data[0].latitude, data[0].longitude], 15);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: copyright,
+        attribution:    "'Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, " +
+                        "<a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, " +
+                        "Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>',",
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: leafyToken
+        accessToken: "pk.eyJ1IjoibWFpbm5pbmUiLCJhIjoiY2sxeGg2cDQ3MGJtdjNvcWd4aWplMnFtMCJ9.6QkdiBXn6IMmw8MFEe455g"
     }).addTo(map);
 
     for (let i = 0; i < data.length; i++) {
